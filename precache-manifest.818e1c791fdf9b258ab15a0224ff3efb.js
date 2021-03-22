4d68c0a156bbce4022cdd3c28d6a7f8b309f7c2b
Milan Tagline <milanc.tagline@gmail.com>
2021-03-22 18:00:45 +0530

    Updates

---
+++ precache-manifest.818e1c791fdf9b258ab15a0224ff3efb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0945d0066809373935cd1f77466e9115",
+    "url": "/PWA/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/PWA/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5b2f60c306e574806359",
+    "url": "/PWA/static/css/main.83b42b3b.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/PWA/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PWA/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2f60c306e574806359",
+    "url": "/PWA/static/js/main.f22a8fd3.chunk.js"
+  },
+  {
+    "revision": "7627309718546d5816d3",
+    "url": "/PWA/static/js/runtime-main.dada05b4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
